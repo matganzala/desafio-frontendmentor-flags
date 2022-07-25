fa45bb353e6f598282420773f925b4ab5ce3edc0
matganzala <ganzalamatheus@gmail.com>
2022-07-25 17:23:58 -0300

    add modal

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,31 +1,25 @@
-import { Input } from "@chakra-ui/react";
+import { Input, useDisclosure } from "@chakra-ui/react";
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    Button,
+  } from '@chakra-ui/react';
 import React, { useEffect, useState } from "react";
 //import { Button  } from "react-bootstrap";
 import { Card } from "react-bootstrap";
 import { Navbar } from "../components/navbar";
 import Select from 'react-select';
 import { Link } from 'react-router-dom';
-import Modal from '@mui/material/Modal';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 
-const style = {
-    position: 'absolute' as 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
-  };
-
 export function Home() {
-    const [open, setOpen] = React.useState(false);
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
+    
 
     const options = [
         { value: 'Africa', label: 'Africa'},
@@ -38,6 +32,13 @@ export function Home() {
     const [filterRegion, setFilterRegion] = useState("");
     const [countries, setCountries] = useState([]);
     const [search, setSearch] = useState("");
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const [size, setSize] = useState('full');
+
+    const handleSizeClick = (newSize: any) => {
+        setSize(newSize)
+        onOpen()
+    }
 
     useEffect(() => {
         var requestOptions: any = {
@@ -53,58 +54,75 @@ export function Home() {
             .catch(error => console.log("Erro", error));
     }, [])
 
-    
     return(     
         <>
-            
             <Navbar/>
-            <div className="container">
-                <div className="content-homepage">
-                    <div className="row justify-content-around">
-                        <div className="col-6 col-md-5 mt-3">
-                            <Input type="text" 
-                                className="input-search" 
-                                placeholder='Search for a country' 
-                                onChange = {(e: any) => setSearch(e.target.value)}
-                                />
+                <div className="container">
+                    <div className="content-homepage">
+                        <div className="row justify-content-around">
+                            <div className="col-6 col-md-5 mt-3">
+                                <Input type="text" 
+                                    className="input-search" 
+                                    placeholder='Search for a country' 
+                                    onChange = {(e: any) => setSearch(e.target.value)}
+                                    />
+                            </div>
+                            <div className="col-6  col-md-3 mt-3">
+                                <Select options={options} onChange={(e : any) => setFilterRegion(e.value)}/>
+                            </div>
                         </div>
-                        <div className="col-6  col-md-3 mt-3">
-                            <Select options={options} onChange={(e : any) => setFilterRegion(e.value)}/>
-                        </div>
-                    </div>
-                    <div className="row mt-3">
-                        <div className="col d-flex flex-wrap justify-content-center">
-                           {countries.filter((item: any) => 
-                           item?.name.official.toLowerCase().includes(search.toLowerCase())
-                           )
-                           .filter((item: any) => {
-                                if(filterRegion == ""){
+                        <div className="row mt-3">
+                            <div className="col d-flex flex-wrap justify-content-center">
+                            {countries.filter((item: any) => 
+                            item?.name.official.toLowerCase().includes(search.toLowerCase())
+                            )
+                            .filter((item: any) => {
+                                    if(filterRegion == ""){
 
-                                    return item;
+                                        return item;
 
-                                }else if (item?.region == filterRegion){
-                                    return item;
-                                }
-                           }
-                           ).map((item: any, index: any) => {
-                                return(
-                                    <button onClick={handleOpen} key={index}>
-                                        <Card className="mt-3 mx-3" style={{ minHeight: '20rem', maxHeight: '20rem', width: '18rem' }}>
-                                            <Card.Img variant="top" src={item?.flags.svg} />
-                                            <Card.Body>
-                                                <Card.Title><strong>{item?.name.official}</strong></Card.Title>
-                                                    <p><strong>Population: </strong>{item?.population}</p>
-                                                    <p><strong>Region: </strong>{item?.region}</p>
-                                                    <p><strong>Capital: </strong>{item?.capital}</p>
-                                            </Card.Body>
-                                        </Card>
-                                    </button>
-                                )
-                            })}
+                                    }else if (item?.region == filterRegion){
+                                        return item;
+                                    }
+                            }
+                            ).map((item: any, index: any) => {
+                                    return(
+                                            <>
+                                                {/* <Modal isOpen={isOpen} size={size} onClose={onClose}>
+                                                    <ModalOverlay />
+                                                    <ModalContent>
+                                                        <ModalHeader>{item?.name.common}</ModalHeader>
+                                                        <ModalCloseButton />
+                                                        <ModalBody>
+                                                            2
+                                                        </ModalBody>
+
+                                                        <ModalFooter>
+                                                            <Button colorScheme='blue' mr={3} onClick={onClose}>
+                                                                Close
+                                                            </Button>
+                                                            <Button variant='ghost'>Secondary Action</Button>
+                                                        </ModalFooter>
+                                                    </ModalContent>
+                                                </Modal> */}
+                                                <button onClick={onOpen} key={index}>
+                                                    <Card className="mt-3 mx-3" style={{ minHeight: '20rem', maxHeight: '20rem', width: '18rem' }}>
+                                                        <Card.Img variant="top" src={item?.flags.svg} />
+                                                        <Card.Body>
+                                                            <Card.Title><strong>{item?.name.official}</strong></Card.Title>
+                                                            <p><strong>Population: </strong>{item?.population}</p>
+                                                            <p><strong>Region: </strong>{item?.region}</p>
+                                                            <p><strong>Capital: </strong>{item?.capital}</p>
+                                                        </Card.Body>
+                                                    </Card>
+                                                </button>
+                                            </>
+                                    )
+                                })}
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
         </>
     )
 }
