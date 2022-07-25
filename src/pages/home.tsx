b652cf157676d365a0a578ed19c1d1f08f1ea85d
matganzala <ganzalamatheus@gmail.com>
2022-07-25 15:24:43 -0300

    fix filter

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,13 +1,24 @@
-import { Input, Select } from "@chakra-ui/react";
+import { Input } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import { Button  } from "react-bootstrap";
 import { Card } from "react-bootstrap";
 import { Navbar } from "../components/navbar";
-//import Select from 'react-select';
+import Select from 'react-select'
 
 export function Home() {
+    const options = [
+        { value: 'Africa', label: 'Africa'},
+        { value: 'Americas', label: 'Americas' },
+        { value: 'Europe', label: 'Europe' },
+        { value: 'Asia', label: 'Asia' },
+        { value: 'Oceania', label: 'Oceania' },
+        { value: '', label: 'No filter'},
+    ]
+    const [filterRegion, setFilterRegion] = useState("");
     const [countries, setCountries] = useState([]);
     const [search, setSearch] = useState("");
+
+    
 
     useEffect(() => {
         var requestOptions: any = {
@@ -21,16 +32,11 @@ export function Home() {
             setCountries(result);
           })
             .catch(error => console.log("Erro", error));
-
     }, [])
-
-    const setRegion = () => {
-        
-    }
-
+    
     return(     
         <>
-            <Navbar/>
+            {/* <Navbar/> */}
             <div className="container">
                 <div className="content-homepage">
                     <div className="row ">
@@ -42,34 +48,36 @@ export function Home() {
                                 />
                         </div>
                         <div className="col-12 col-md-4 mt-3">
-                            <Select >
-                                <option></option>w
-                            </Select>
+                            <Select options={options} onChange={(e : any) => setFilterRegion(e.value)}/>
                         </div>
                     </div>
                     <div className="row mt-3">
                         <div className="col d-flex flex-wrap justify-content-center">
-                           {countries.filter((item) => 
-                           item?.name.official.toLowerCase().includes(search)
+                           {countries.filter((item: any) => 
+                           item?.name.official.toLowerCase().includes(search.toLowerCase())
+                           )
+                           .filter((item: any) => {
+                                if(filterRegion == ""){
+                                    
+                                    return item;
+
+                                }else if (item?.region == filterRegion){
+                                    return item;
+                                }
+                           }
                            ).map((item: any, index: any) => {
                                 return(
-                                    <>
-                                        <button>
-                                            <Card key={item?.name.official} className="mt-3 mx-3" style={{ width: '15rem' }}>
-                                                <Card.Img variant="top" src={item?.flags.svg} />
-                                                <Card.Body>
-                                                    <Card.Title><strong>{item?.name.official}</strong></Card.Title>
-                                                        <Card.Text>
-                                                            <>
-                                                                <p><strong>Population: </strong>{item?.population}</p>
-                                                                <p><strong>Region: </strong>{item?.region}</p>
-                                                                <p><strong>Capital: </strong>{item?.capital}</p>
-                                                            </>
-                                                        </Card.Text>
-                                                </Card.Body>
-                                            </Card>
-                                        </button>
-                                    </>
+                                    <a key={index}>
+                                        <Card  className="mt-3 mx-3" style={{ width: '15rem' }}>
+                                            <Card.Img variant="top" src={item?.flags.svg} />
+                                            <Card.Body>
+                                                <Card.Title><strong>{item?.name.official}</strong></Card.Title>
+                                                    <p><strong>Population: </strong>{item?.population}</p>
+                                                    <p><strong>Region: </strong>{item?.region}</p>
+                                                    <p><strong>Capital: </strong>{item?.capital}</p>
+                                            </Card.Body>
+                                        </Card>
+                                    </a>
                                 )
                             })}
                         </div>
