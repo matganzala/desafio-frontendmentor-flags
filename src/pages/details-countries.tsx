dd1c5522fa41398497da308accc2ea4209cc07f9
Matheus Ganzala <ganzalamatheus@gmail.com>
2022-10-23 12:51:44 -0300

    fixar botões para alternar entre fronteiras

--- src/pages/details-countries.tsx
+++ src/pages/details-countries.tsx
@@ -6,7 +6,7 @@ import { Navbar } from "../components/navbar";
 
 export function Details() {
     var getDataCountries = JSON.parse(localStorage.getItem('countries')!);
-    const [borders, setBorders] = useState(getDataCountries.borders);
+    const [borders, setBorders] = ("");        
     var navigate = useNavigate();
     console.log(borders);
 
@@ -18,7 +18,7 @@ export function Details() {
         <>
             <div className="content-countries">
                     <Navbar/>                    
-                    <div className="container">
+                    <div className="container" >
                         <div className="row">
                             <div className="col-12 col-md-7 mt-5">
                                 <button 
@@ -32,36 +32,55 @@ export function Details() {
                                         className="img-thumbnail w-100 h-100"
                                         style={{objectFit: 'cover', maxHeight: '300px', maxWidth:' 600px'}}
                                     />
-                                </div>
+                                </div>  
                             </div>
-                            <div className="col-12 col-md-4 d-block justify-content-center">
-                                <div className="row">
-                                    <div className="col-12 col-md-12 d-flex" style={{marginTop: '25px'}}>                                    
+                            <div className="col-12 col-md-4 d-flex justify-content-center mt-lg-5">
+                                <div className="row mt-lg-5">
+                                    <div className=" mt-lg-5 col-12 col-md-6 d-flex" style={{marginTop: '25px'}}>                                    
                                         <span >{getDataCountries?.name.common}</span>
                                     </div>
-                                    <div className="col-12 col-md-12 d-flex mb-5">
+                                    <div className="mt-lg-4 col-12 col-md-12 d-flex mb-5">
                                         <div className=" d-flex flex-column">
                                             <div className="mt-1">
+                                                <span>Name: </span>
                                                 <span>{getDataCountries?.name.official}</span>
                                             </div>
                                             <div className="mt-1">
-                                                <span>{getDataCountries?.population}</span>
+                                                <span>Population: </span>
+                                                <span>{getDataCountries?.population.toLocaleString('pt-BR')}</span>
                                             </div>
                                             <div className="mt-1">
+                                                <span>Region: </span>
                                                 <span>{getDataCountries?.region}</span>
                                             </div>
                                             <div className="mt-1">
+                                                <span>SubRegion: </span>
                                                 <span>{getDataCountries?.subRegion}</span>
                                             </div>
                                             <div className="mt-1">
+                                                <span>Capital: </span>
                                                 <span>{getDataCountries?.capital}</span>
                                             </div>
+                                            <div className="mt-3">
+                                                <span>Borders: </span>
+                                            </div>
+                                            <div className=" d-flex flex-wrap">                                                                                                
+                                                {getDataCountries.borders.map((border: string) => {
+                                                    return (
+                                                        <div className="border-button mt-2 ms-1 d-flex flex-wrap align-items-center justify-content-center">
+                                                            <span>{border}</span>
+                                                        </div> 
+                                                    ) 
+                                                })}
+                                            </div>
                                         </div> 
-                                        <div className=" ms-5 d-flex flex-column">
+                                        <div className="d-flex flex-column">
                                             <div className="mt-1">
+                                                <span>Tld: </span>
                                                 <span className="mt-1">{getDataCountries?.tld}</span>                                            
                                             </div>
-                                            <div className="mt-1">
+                                            <div className="mt-1 d-flex">
+                                                <span className="mt-1 me-1">Currencies: </span>
                                                 <span className="mt-1">
                                                     {Object.values(getDataCountries.currencies).map((coin: any) => {
                                                         return coin.name;
@@ -70,17 +89,16 @@ export function Details() {
                                             </div>
                                             <div className="mt-1">
                                                 <span className="mt-1">
+                                                    <span>Language: </span>
                                                     {Object.values(getDataCountries.languages).map((language: any) => {
-                                                        return language;
+                                                        return (<span className="mx-1">{language}</span>);
                                                     })};
                                                 </span>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                                
-                            </div>
-                                
+                            </div>  
                         </div>
                     </div>
                     
